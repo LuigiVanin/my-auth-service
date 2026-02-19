529124c2fa3934c4e46121ebee0768e4d75b2449
LuigiVanin <luisfvaninmartins@gmail.com>
2026-02-18 22:09:29 -0300

    tests: initializating tests using testify

---
+++ tests/modules/login/login.service_test.go
@@ -0,0 +1,124 @@
+package login_test
+
+import (
+	e "auth_service/app/errors"
+	"auth_service/app/models/dto"
+	"auth_service/app/modules/authorize/services"
+	ls "auth_service/app/modules/login/services"
+	entity "auth_service/infra/entities"
+
+	"testing"
+
+	mock "auth_service/tests/modules/mock"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"go.uber.org/zap"
+)
+
+// --- Test Suite ---
+
+type LoginWithPasswordServiceTestSuite struct {
+	suite.Suite
+	mockUserRepo       *mock.MockUserRepository
+	mockHashService    *mock.MockHashService
+	mockSessionService *mock.MockSessionService
+	mockAuthService    *mock.MockAuthorizeService
+	mockOtpService     *mock.MockOtpService
+
+	loginService ls.ILoginService
+}
+
+func (this *LoginWithPasswordServiceTestSuite) SetupTest() {
+	this.mockUserRepo = new(mock.MockUserRepository)
+	this.mockHashService = new(mock.MockHashService)
+	this.mockSessionService = new(mock.MockSessionService)
+	this.mockAuthService = new(mock.MockAuthorizeService)
+	this.mockOtpService = new(mock.MockOtpService)
+	logger := zap.NewNop()
+
+	this.loginService = ls.NewLoginService(
+		this.mockUserRepo,
+		this.mockHashService,
+		this.mockSessionService,
+		this.mockAuthService,
+		this.mockOtpService,
+		logger,
+	)
+}
+
+func (this *LoginWithPasswordServiceTestSuite) TestLoginOnAppWithoutPasswordLoginType_Error() {
+	app := &entity.App{
+		LoginTypes: []string{"WITH_OTP"},
+	}
+	userData := dto.LoginPayloadWithPassoword{}
+	requestInfo := dto.RequestInfo{}
+
+	response, err := this.loginService.LoginWithPassword(app, userData, requestInfo)
+
+	assert.Nil(this.T(), response)
+	assert.Error(this.T(), err)
+	assert.IsType(this.T(), e.ThrowNotAllowed(""), err)
+
+	if ge, ok := err.(*e.GlobalError); ok {
+
+		assert.Equal(this.T(), 405, ge.Code.Second)
+		assert.Equal(
+			this.T(),
+			e.GlobalErrorCode("NOT_ALLOWED"),
+			ge.Code.First,
+		)
+	}
+}
+
+func (this *LoginWithPasswordServiceTestSuite) TestLoginWithNonExistentUserOnTheApp_Error() {
+
+}
+
+func (this *LoginWithPasswordServiceTestSuite) TestLogin_Success() {
+	// Arrange
+	app := &entity.App{
+		ID:         "app-id",
+		LoginTypes: []string{"WITH_PASSWORD"},
+		UsersPool:  entity.UsersPool{ID: "pool-id"},
+	}
+	payload := dto.LoginPayloadWithPassoword{
+		Email:    "test@example.com",
+		Password: "password",
+	}
+	requestInfo := dto.RequestInfo{}
+
+	user := &entity.User{
+		ID:           1,
+		Email:        "test@example.com",
+		PasswordHash: "hashed_password",
+	}
+	session := &entity.Session{ID: "session-id"}
+	credentials := &services.AuthorizationCredentials{
+		AccessToken:  "access-token",
+		RefreshToken: "refresh-token",
+	}
+
+	// Mocks
+	expectedWhere := entity.User{
+		Email:       "test@example.com",
+		UsersPoolId: "pool-id",
+	}
+	this.mockUserRepo.On("FindWhere", expectedWhere, []string{"Profile"}).Return(user, nil)
+	this.mockHashService.On("Compare", "password", "hashed_password").Return(true, nil)
+	this.mockSessionService.On("CreateNew", app, user, requestInfo, "WITH_PASSWORD").Return(session, nil)
+	this.mockAuthService.On("CreateAuthorizationCredentials", app, session).Return(credentials, nil)
+
+	// Act
+	response, err := this.loginService.LoginWithPassword(app, payload, requestInfo)
+
+	// Assert
+	assert.NoError(this.T(), err)
+	assert.NotNil(this.T(), response)
+	assert.Equal(this.T(), "session-id", response.SessionId)
+	assert.Equal(this.T(), "access-token", response.AccessToken)
+}
+
+func TestServiceSuite(t *testing.T) {
+	suite.Run(t, new(LoginWithPasswordServiceTestSuite))
+}
